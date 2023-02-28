2ee71fcb7bd082a243a51149d6b9d3394bbe0f9e
Felender <felender7@gmail.com>
2023-03-01 00:32:40 +0200

    added javascript render on hero section

---
+++ public/assets/js/app.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+
+//HERO SECTION
+let heroSubTitle = "A history of everything you copy";
+let heroDescription = "Clipboard allows you to track and organize everything you copy. </br>Instantly access your clipboard on all your devices"
+let heroLinks =  '<a href="#" class="btn btn-success btn-lg shadow-sm">Download for iOS </a> <a href="#" class="btn btn-primary btn-lg shadow-sm">Download for Mac</a>'
+
+document.getElementById("hero-logo").src = "assets/img/logo.svg";
+document.getElementById("hero-subtitle").innerHTML = heroSubTitle;
+document.getElementById("hero-description").innerHTML = heroDescription;
+document.getElementById("hero-links").innerHTML = heroLinks
+
+
+}
+
+
+
