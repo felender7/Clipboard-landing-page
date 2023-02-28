67d8bee78b4551ac09e438a5674ca00c7e2d1cf6
Felender <felender7@gmail.com>
2023-03-01 00:45:43 +0200

    added javascript render on tracking  section

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11,7 +11,19 @@ document.getElementById("hero-subtitle").innerHTML = heroSubTitle;
 document.getElementById("hero-description").innerHTML = heroDescription;
 document.getElementById("hero-links").innerHTML = heroLinks
 
+//KEEP TRACT SECTION
+let trackTitle = "Keep track of your snippets Clipboard";
+let trackDescription = `Clipboard
+            instantly stores any item you copy in the cloud, meaning you can access <br> your
+            snippets immediately on all your devices. Our Mac and iOS apps will help you <br>
+            organize everything`
 
+let quickSearchTitle = "Quick Search";
+let quickSearchDescription = "Easily search your snippets by content,category, web address, application, and more"
+
+document.getElementById("track-image").src="assets/img/image-computer.png";
+document.getElementById("quick-search-title").innerHTML = quickSearchTitle;
+document.getElementById("quick-search-description").innerHTML = quickSearchDescription;
 }
 
 
