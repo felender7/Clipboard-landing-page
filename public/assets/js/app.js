dc70820c9a9ef646e3c06278c855b5f2d659583d
felender7 <felender7@gmail.com>
2023-03-01 11:11:08 +0200

    rendered the rest of the section in javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -36,6 +36,47 @@ document.getElementById("icloud-sync-title").innerHTML = icloudSyncTitle;
 document.getElementById("icloud-sync-description").innerHTML = icloudSyncDescription;
 document.getElementById("complete-history-title").innerHTML = completeHistory;
 document.getElementById("complete-history-desc").innerHTML = completeHistoryDesc;
+
+//ACCESS CLIPBOARD SECTION
+
+let accessClipboardTitle ="Access Clipboard anywhere" ;
+let accessClipboardDesc ="Whether youre on the go, or at your computer, you can access all your Clipboard <br> snippets in a few simple clicks." ;
+let accessClipboardImg ="assets/img/image-devices.png";
+
+document.getElementById("access-clipboard-title").innerHTML = accessClipboardTitle;
+document.getElementById("access-clipboard-desc").innerHTML = accessClipboardDesc;
+document.getElementById("access-clipboard-img").src= accessClipboardImg;
+
+//WORKFLOW SECTION
+let superchargeTitle = "Supercharge your workflow";
+let superchargeDesc = 'We’ve got the tools to boost your productivity';
+
+document.getElementById("supercharge-title").innerHTML = superchargeTitle;
+document.getElementById("supercharge-desc").innerHTML=superchargeDesc;
+
+document.getElementById("3-cols").innerHTML = 
+`
+<div class="col">
+<img src="assets/img/icon-blacklist.svg" alt="" class="mb-5">
+<h4>Create blacklists</h4>
+<p>Ensure sensitive information never makes its way to your clipboard by excluding certain sources</p>
+</div>
+
+<div class="col">
+<img src="assets/img/icon-text.svg" alt="" class="mb-5">
+<h4>Plain text snippets</h4>
+<p>Remove unwanted formatting from copied
+    text for a consistent look.</p>
+</div>
+
+<div class="col">
+<img src="assets/img/icon-preview.svg" alt="" class="mb-5">
+<h4>Sneak preview </h4>
+    <p>Quick preview of all snippets on your Clipboard for easy access</p>
+</div>
+
+`
+
 }
 
 
