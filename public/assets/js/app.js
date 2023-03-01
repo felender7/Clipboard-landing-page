de2697ae249624a7dd1515c504a14c2e47c80b8f
felender7 <felender7@gmail.com>
2023-03-01 10:14:05 +0200

    updated track section to render in javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,14 +16,26 @@ let trackTitle = "Keep track of your snippets Clipboard";
 let trackDescription = `Clipboard
             instantly stores any item you copy in the cloud, meaning you can access <br> your
             snippets immediately on all your devices. Our Mac and iOS apps will help you <br>
-            organize everything`
+            organize everything`;
 
 let quickSearchTitle = "Quick Search";
-let quickSearchDescription = "Easily search your snippets by content,category, web address, application, and more"
+let quickSearchDescription = "Easily search your snippets by content,category, web address, application, and more";
+let icloudSyncTitle = "iCloud Sync";
+let icloudSyncDescription ="Instantly saves and syncs snippets across all your devices" ;
+let completeHistory = "Complete History";
+let completeHistoryDesc = "Retrieve any snippets from the first moment you started using the app"
 
+
+document.getElementById("tract-title").innerHTML = trackTitle;
+document.getElementById("track-description").innerHTML = trackDescription;
+document.getElementById("track-description")
 document.getElementById("track-image").src="assets/img/image-computer.png";
 document.getElementById("quick-search-title").innerHTML = quickSearchTitle;
 document.getElementById("quick-search-description").innerHTML = quickSearchDescription;
+document.getElementById("icloud-sync-title").innerHTML = icloudSyncTitle;
+document.getElementById("icloud-sync-description").innerHTML = icloudSyncDescription;
+document.getElementById("complete-history-title").innerHTML = completeHistory;
+document.getElementById("complete-history-desc").innerHTML = completeHistoryDesc;
 }
 
 
